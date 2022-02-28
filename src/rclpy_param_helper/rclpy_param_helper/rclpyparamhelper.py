273be0f3a801855e9d42531cc82474efb0ac35c9
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-02-28 11:53:46 -0500

    changed to set_parameters

--- src/rclpy_param_helper/rclpy_param_helper/rclpyparamhelper.py
+++ src/rclpy_param_helper/rclpy_param_helper/rclpyparamhelper.py
@@ -4,6 +4,8 @@ https://docs.ros2.org/latest/api/rclpy/api/parameters.html
 ROS2 doesn't accept matrices as parameters, so we declare a parameter named "<name>___shape" with the shape.
 Arrays (and matrices) are converted to numpy arrays and they MUST use only one type: int, float, string, bool or byte.
 https://numpy.org/doc/stable/reference/generated/numpy.dtype.char.html
+
+It expects the node to set 'allow_undeclared_parameters=True'.
 """
 import rclpy
 from rclpy.parameter import Parameter
@@ -19,48 +21,64 @@ for ti in Parameter.Type:
     int2type[ti.value] = ti.name
 
 def Dict2ROS2Params(node, dictparams):
+    params = []
     for k,v in dictparams.items():
         name = k
         value = v
         value_type = str(type(value)).lower()
 
         if 'None' in value_type:
-            node.declare_parameter(name, descriptor=ParameterDescriptor(name=name, type=type2int['NOT_SET']))
+            params.append(Parameter(name=name, type_=Parameter.Type.NOT_SET, value=value))
+            # node.declare_parameter(name, descriptor=ParameterDescriptor(name=name, type=type2int["NOT_SET"]))
         elif 'bool' in value_type:
-            node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["BOOL"]))
+            params.append(Parameter(name=name, type_=Parameter.Type.BOOL, value=value))
+            # node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["BOOL"]))
         elif 'int' in value_type:
-            node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["INTEGER"]))
+            params.append(Parameter(name=name, type_=Parameter.Type.INTEGER, value=value))
+            # node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["INTEGER"]))
         elif 'float' in value_type:
-            node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["DOUBLE"]))
+            params.append(Parameter(name=name, type_=Parameter.Type.DOUBLE, value=value))
+            # node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["DOUBLE"]))
         elif 'str' in value_type:
-            node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["STRING"]))
+            params.append(Parameter(name=name, type_=Parameter.Type.STRING, value=value))
+            # node.declare_parameter(name, value=value, descriptor=ParameterDescriptor(name=name, type=type2int["STRING"]))
         elif 'ndarray' in value_type or 'list' in value_type or 'tuple' in value_type:
-            temp_value = np.array(value)
-            value_shape = temp_value.shape
+            array_value = np.array(value)
+            list_value = array_value.ravel().tolist()
+            value_shape = array_value.shape
             shape_descr = name + '___shape'
-            node.declare_parameter(shape_descr, value=value_shape, descriptor=ParameterDescriptor(name=name, type=type2int["INTEGER_ARRAY"]))
-            char_dtype = temp_value.dtype.char
+            params.append(Parameter(name=shape_descr, type_=Parameter.Type.INTEGER_ARRAY, value=value_shape))
+            # node.declare_parameter(shape_descr, value=value_shape, descriptor=ParameterDescriptor(name=name, type=type2int["INTEGER_ARRAY"]))
+            char_dtype = array_value.dtype.char
             if   char_dtype.lower()=='l' or char_dtype.lower()=='i' or char_dtype.lower()=='h'  or char_dtype.lower()=='b':
                 # uint becomes int...
-                param_descriptor = ParameterDescriptor(name=name, type=type2int["INTEGER_ARRAY"])
+                params.append(Parameter(name=name, type_=Parameter.Type.INTEGER_ARRAY, value=list_value))
+                # param_descriptor = ParameterDescriptor(name=name, type=type2int["INTEGER_ARRAY"])
             elif char_dtype=='d' or char_dtype=='f' or char_dtype=='e':
-                param_descriptor = ParameterDescriptor(name=name, type=type2int["DOUBLE_ARRAY"])
+                params.append(Parameter(name=name, type_=Parameter.Type.DOUBLE_ARRAY, value=list_value))
+                # param_descriptor = ParameterDescriptor(name=name, type=type2int["DOUBLE_ARRAY"])
             elif char_dtype=='U':
-                param_descriptor = ParameterDescriptor(name=name, type=type2int["STRING_ARRAY"])
+                params.append(Parameter(name=name, type_=Parameter.Type.STRING_ARRAY, value=list_value))
+                # param_descriptor = ParameterDescriptor(name=name, type=type2int["STRING_ARRAY"])
             elif char_dtype=='?':
-                param_descriptor = ParameterDescriptor(name=name, type=type2int["BOOL_ARRAY"])
+                params.append(Parameter(name=name, type_=Parameter.Type.BOOL_ARRAY, value=list_value))
+                # param_descriptor = ParameterDescriptor(name=name, type=type2int["BOOL_ARRAY"])
             elif char_dtype=='S':
-                param_descriptor = ParameterDescriptor(name=name, type=type2int["BYTE_ARRAY"])
+                params.append(Parameter(name=name, type_=Parameter.Type.BYTE_ARRAY, value=list_value))
+                # param_descriptor = ParameterDescriptor(name=name, type=type2int["BYTE_ARRAY"])
             else:
-                node.get_logger().error(f"Array of {temp_value.dtype} is not supported!")
-                raise TypeError(f"Array of {temp_value.dtype} is not supported!")
+                node.get_logger().error(f"Array of {array_value.dtype} is not supported!")
+                raise TypeError(f"Array of {array_value.dtype} is not supported!")
 
-            node.declare_parameter(name, value=temp_value.ravel().tolist(), descriptor=param_descriptor)
+            # node.declare_parameter(name, value=list_value, descriptor=param_descriptor)
         else:
             node.get_logger().error(f"{value_type} is not supported!")
             raise TypeError(f"{value_type} is not supported!")
         
         node.get_logger().info(f"{name} : {value} set as a parameter!")
+
+    node.set_parameters(params)
+
 
 
 def ROS2Params2Dict(node, node_name, parameter_names):
