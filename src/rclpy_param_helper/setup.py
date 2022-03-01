c1d4fd112d45d5e0d500f6d10c885a9598562138
ricardodeazambuja <ricardo.azambuja@gmail.com>
2022-03-01 11:44:38 -0500

    updating info

--- src/rclpy_param_helper/setup.py
+++ src/rclpy_param_helper/setup.py
@@ -4,18 +4,18 @@ package_name = 'rclpy_param_helper'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'numpy', 'rclpy', 'ros2param'],
     zip_safe=True,
-    maintainer='ros2user',
-    maintainer_email='ros2user@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Ricardo de Azambuja',
+    maintainer_email='ricardo.azambuja@gmail.com',
+    description='Convert between Python dictionary and ROS2 parameters',
+    license='MIT',
     tests_require=['pytest'],
 )
